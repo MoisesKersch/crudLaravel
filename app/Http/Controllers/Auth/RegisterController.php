efcb19751bd42de502f1f832c4ae4b1e08a22cb8
MoisesKersch <lightspeedoff@gmail.com>
2018-03-11 21:58:27 -0300

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,8 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    
+    protected $redirectTo = 'clientes';
 
     /**
      * Create a new controller instance.
